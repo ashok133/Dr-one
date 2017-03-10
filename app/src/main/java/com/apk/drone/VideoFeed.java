26f3c945d729c013fd59003d46d34bb9fd73dd25
Ashok Patel Apk <helloapk5896@gmail.com>
2017-03-10 11:14:34 +0530

    init

---
+++ app/src/main/java/com/apk/drone/VideoFeed.java
@@ -0,0 +1,66 @@
+package com.apk.drone;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+/**
+ * Created by apk on 09/03/17.
+ */
+
+public class VideoFeed extends ActionBarActivity {
+
+    EditText addrField;
+    Button btnConnect;
+    VideoView streamView;
+    MediaController mediaController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addrField = (EditText)findViewById(R.id.addr);
+        btnConnect = (Button)findViewById(R.id.connect);
+        streamView = (VideoView)findViewById(R.id.streamview);
+
+        btnConnect.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String s = addrField.getEditableText().toString();
+                playStream(s);
+            }});
+
+    }
+
+    private void playStream(String src){
+        Uri UriSrc = Uri.parse(src);
+        if(UriSrc == null){
+            Toast.makeText(VideoFeed.this,
+                    "UriSrc == null", Toast.LENGTH_LONG).show();
+        }else{
+            streamView.setVideoURI(UriSrc);
+            mediaController = new MediaController(this);
+            streamView.setMediaController(mediaController);
+            streamView.start();
+
+            Toast.makeText(VideoFeed.this,
+                    "Connect: " + src,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        streamView.stopPlayback();
+    }
+
+}
